570ec50bddd80b03ca5f30277dc76f44c721eba1
Charlie Anderson <Charlie@eduroam-166-179.dyn.carleton.edu>
2017-10-22 16:16:09 -0500

    Adding basic code

---
+++ baebot.py
@@ -0,0 +1,29 @@
+'''
+Created by Charlie, Ammar, and Leila
+This is the source code for our chatbot BaeBot
+'''
+
+import nltk
+
+#Tokenizes the response given by the user.
+def tokenize(response):
+	tokens = nltk.word_tokenize(response)
+	tagged = nltk.pos_tag(tokens)
+	print tagged #TEMPORARY, should return tagged
+
+
+#main function, provides loop that allows for the back and forth between BaeBot and the conversation
+def main():
+	print "hey bb, hows it going?"
+	convoComplete = True
+	while(convoComplete): #loop until conversation is satisfied
+		baeBotResponse = "sup"
+		n = raw_input(baeBotResponse + "\n") # prints Baebot question, takes input from user
+		if (n == 'not much'): # TEMPORARY, should evaluate response
+			convoComplete = False
+			tokenize(n)
+		
+
+
+#Initiate convo
+main()
