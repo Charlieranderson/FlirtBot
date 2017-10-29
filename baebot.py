7f779a45d877cc35ea8675437e2558ddd775f41a
Ammar Babar <ammarbabar@eduroam-162-96.dyn.carleton.edu>
2017-10-29 17:21:55 -0500

    More pseudo code than actual code

--- baebot.py
+++ baebot.py
@@ -4,6 +4,10 @@ This is the source code for our chatbot BaeBot
 '''
 
 import nltk
+import random
+#import narrativeStuff
+
+
 
 #Tokenizes the response given by the user.
 def tokenize(response):
@@ -12,18 +16,34 @@ def tokenize(response):
 	print tagged #TEMPORARY, should return tagged
 
 
+def pickOpener():
+	openerNum = random.randint(1,4)
+	return #some sentence FIXIT
+
+def makeFlirt(response, convoComplete, flirtyWeight):
+	'''
+	1. Tag response ... tokenize
+	2. Sentiment analysis http://www.nltk.org/howto/sentiment.html does it help???
+	3. amI... 
+	3.5. based on sentiment and keywords, look for response in response dict, return that
+	'''
+
+def amIDealingWithADickhead(flirtyWeight):
+	#if a dickhead, end convo
+	#else: keep going
+
 #main function, provides loop that allows for the back and forth between BaeBot and the conversation
 def main():
-	print "hey bb, hows it going?"
 	convoComplete = True
+	flirtyWeight = 0
+	baeBotResponse = pickOpener()
 	while(convoComplete): #loop until conversation is satisfied
-		baeBotResponse = "sup"
-		n = raw_input(baeBotResponse + "\n") # prints Baebot question, takes input from user
-		if (n == 'not much'): # TEMPORARY, should evaluate response
-			convoComplete = False
-			tokenize(n)
-		
+		response = raw_input(baeBotResponse + "\n") # prints Baebot question, takes input from user
+		baeBotResponse = makeFlirt(response)
+
+
+
 
 
 #Initiate convo
-main()
+tokenize("My Name is Ammar.")
