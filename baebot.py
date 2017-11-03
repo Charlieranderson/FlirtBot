eff2ada53f05128d3f0e4948f1b35046ecf679e2
Tristan Willard <tristanwillard@eduroam-174-151.dyn.carleton.edu>
2017-11-03 16:52:34 -0500

    Adding changes relating to sentiment analysis

--- baebot.py
+++ baebot.py
@@ -4,7 +4,10 @@ This is the source code for our chatbot BaeBot
 '''
 
 import nltk
+from nltk.tokenize import word_tokenize
 import random
+import ast
+import pickle
 #import narrativeStuff
 
 
@@ -28,22 +31,49 @@ def makeFlirt(response, convoComplete, flirtyWeight):
 	3.5. based on sentiment and keywords, look for response in response dict, return that
 	'''
 
-def amIDealingWithADickhead(flirtyWeight):
+#def amIDealingWithADickhead(flirtyWeight):
 	#if a dickhead, end convo
 	#else: keep going
 
+def analyze(sentence, classifier, dictionary):
+	sentence_features = {word.lower(): (word in word_tokenize(sentence.lower())) for word in dictionary}
+	return classifier.classify(sentence_features)
+
+def getClassifier():
+	training_data_file = "training_data.txt"
+	dictionary_file = "dictionary.txt"
+	training_data = open(training_data_file,"r")
+	dictionary_data = open(dictionary_file,"r")
+	data = training_data.readline()
+	dictionary_string = dictionary_data.readline()
+	dictionary = ast.literal_eval(dictionary_string)
+	training_data_list = ast.literal_eval(data)
+	classifier = nltk.NaiveBayesClassifier.train(training_data_list)
+	with open('classifier.txt', 'wb') as classifier_file:
+		pickle.dump(classifier, classifier_file)
+	with open('dict.txt', 'wb') as dict_file:
+		pickle.dump(dictionary, dict_file)
+
 #main function, provides loop that allows for the back and forth between BaeBot and the conversation
 def main():
-	convoComplete = True
-	flirtyWeight = 0
-	baeBotResponse = pickOpener()
-	while(convoComplete): #loop until conversation is satisfied
-		response = raw_input(baeBotResponse + "\n") # prints Baebot question, takes input from user
-		baeBotResponse = makeFlirt(response)
+	with open('classifier.txt', 'rb') as classifier_file:
+		classifier = pickle.load(classifier_file)
+	with open('dict.txt', 'rb') as dict_file:
+		dictionary = pickle.load(dict_file)
+	result = analyze("You are beautiful!", classifier, dictionary)
+	print result
+	result2 = analyze("You are so ugly.", classifier, dictionary)
+	print result2
+#	convoComplete = True
+#	flirtyWeight = 0
+#	baeBotResponse = pickOpener()
+#	while(convoComplete): #loop until conversation is satisfied
+#		response = raw_input(baeBotResponse + "\n") # prints Baebot question, takes input from user
+#		baeBotResponse = makeFlirt(response)
+
+if __name__ == "__main__":
+	main()
 
 
-
-
-
-#Initiate convo
-tokenize("My Name is Ammar.")
+##Initiate convo
+#tokenize("My Name is Ammar.")
---
+++ sentiment_analysis.py
@@ -0,0 +1,34 @@
+import nltk
+from nltk.tokenize import word_tokenize
+  
+training_list = []
+flirty_file = "FlirtyTexts.txt"
+not_flirty_file = "Boring_Corpus"
+#training_data_file = "training_data.txt"
+dictionary_file = "dictionary.txt"
+
+flirty = open(flirty_file,"r")
+not_flirty = open(not_flirty_file, "r")
+dictionary_string = open(dictionary_file, "w")
+#training_data = open(training_data_file, "w")
+for line in flirty.readlines():
+    if line != "\n":
+        training_list.append(tuple((line.strip(), "flirty")))
+for line in not_flirty.readlines():
+    if line != "\n":
+        training_list.append(tuple((line.strip(), "not flirty")))
+    
+dictionary = set(word.lower() for passage in training_list for word in word_tokenize(passage[0]))
+dictionary_string.write(str(dictionary))
+print("yo")
+
+t = [({word: (word in word_tokenize(x[0])) for word in dictionary}, x[1]) for x in training_list]
+
+#training_data.write(str(t))
+#
+#classifier = iter(nltk.NaiveBayesClassifier.train(t))
+#  
+#test_data = "Manchurian was hot and spicy"
+#test_data_features = {word.lower(): (word in word_tokenize(test_data.lower())) for word in dictionary}
+#  
+#print (classifier.classify(test_data_features))
